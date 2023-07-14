6788a904f43fe78015155e3045ff4d0a74ed366f
RubenTriana <rubendariotriana@gmail.com>
2023-07-13 19:59:50 -0500

    'Borrado de hora nueva'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,20 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
         public function boot(): void
     {
-        $this->setUTCOffsetInDBConnection();
+
         // Resto del código del método boot()
     }
 
-    private function setUTCOffsetInDBConnection()
-    {
-        $now = new \DateTime();
-        $mins = $now->getOffset() / 60;
-        $sgn = ($mins < 0 ? -1 : 1);
-        $mins = abs($mins);
-        $hrs = floor($mins / 60);
-        $mins -= $hrs * 60;
-        $offset = sprintf('%+d:%02d', $hrs*$sgn, $mins);
-        DB::statement("SET time_zone='".$offset."';");
-    }
+
 
 }
